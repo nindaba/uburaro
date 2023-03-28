dc0458a2affaabb53f1a721436e524503cf94800
Arthur Nindaba <arthur.nindaba@vincit.fi>
2023-03-28 09:20:03 +0300

    final release

--- management-backoffice/electron-app/main.ts
+++ management-backoffice/electron-app/main.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 
 let win: BrowserWindow |null;
 const args = process.argv.slice(1),
-    serve = args.some(val => val === '--serve') || true;
+    serve = args.some(val => val === '--serve');
 
 function createWindow(): BrowserWindow {
 
@@ -14,7 +14,7 @@ function createWindow(): BrowserWindow {
     win = new BrowserWindow({
         height: 800,
         width: 1200,
-        minWidth: 1200,
+        minWidth: 1240,
         center:true,
         webPreferences: {
             nodeIntegration: true,
