50f17d208b00ff76fc0732c3e6c0989d9259fa7b
Arthur Nindaba <arthur.nindaba@vincit.fi>
2023-04-11 08:31:53 +0200

    MB-101 | create pdf endpoints for the reports

--- modules/manager-core/src/main/java/bi/manager/core/repositories/MBRentOrderRepository.java
+++ modules/manager-core/src/main/java/bi/manager/core/repositories/MBRentOrderRepository.java
@@ -11,14 +11,19 @@ import java.time.LocalDate;
 import java.util.Collection;
 
 public interface MBRentOrderRepository extends ItemRepository<MBRentOrderType> {
+    String QUERY = "SELECT c FROM " + MBRentOrderType.ITEM_TYPE + " AS c " +
+            "WHERE c." + MBRentOrderType.RENT_PROPERTY + ".facility.code = ?1 " +
+            "AND (c." + MBRentOrderType.FROM + " <= ?3 " +
+            "AND c." + MBRentOrderType.TO + " >= ?2)";
+
     @Override
     default <TYPE extends ItemType> boolean belongsTo(Class<TYPE> typeClass) {
         return MBRentOrderType.class.equals(typeClass);
     }
 
-    @Query("SELECT c FROM "+ MBRentOrderType.ITEM_TYPE+" AS c WHERE c."+MBRentOrderType.RENT_PROPERTY+".facility.code = ?1 AND (c."+MBRentOrderType.FROM+" <= ?3 AND c."+MBRentOrderType.TO+" >= ?2)")
+    @Query(QUERY)
     Page<MBRentOrderType> findOrdersByFacilityAndDates(String facility, LocalDate from, LocalDate to, Pageable pageable);
 
-    @Query("SELECT c FROM "+ MBRentOrderType.ITEM_TYPE+" AS c WHERE c."+MBRentOrderType.RENT_PROPERTY+".facility.code = ?1 AND (c."+MBRentOrderType.FROM+" <= ?3 AND c."+MBRentOrderType.TO+" >= ?2)")
+    @Query(QUERY)
     Collection<MBRentOrderType> findOrdersByFacilityAndDates(String facility, LocalDate from, LocalDate to);
 }
