8882f815d01d6509a7b8134259ac0fe845c58259
Arthur Nindaba <arthur.nindaba@vincit.fi>
2024-01-03 08:53:55 +0100

    MB-fix-Order | fixed generation of infinite orders on the last month of the contract

---
+++ modules/manager-core/src/main/java/bi/manager/core/strategies/impl/DefaultGenerateRentOrdersStrategy.java
@@ -0,0 +1,84 @@
+package bi.manager.core.strategies.impl;
+
+import bi.manager.core.repositories.MBRentContractRepository;
+import bi.manager.core.services.MBRentOrderService;
+import bi.manager.core.strategies.GenerateRentOrdersStrategy;
+import bi.manager.core.types.client.MBRentContractType;
+import bi.manager.core.types.client.MBRentOrderType;
+import bi.uburaro.core.types.ItemType;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static bi.manager.core.ManagerCoreConstants.RENT_UNIT_SCALE;
+import static java.time.temporal.ChronoUnit.DAYS;
+
+@Service(value = "generateRentOrdersStrategy")
+public class DefaultGenerateRentOrdersStrategy implements GenerateRentOrdersStrategy {
+
+    protected final MBRentContractRepository rentContractRepository;
+    protected final MBRentOrderService rentOrderService;
+
+
+    public DefaultGenerateRentOrdersStrategy(MBRentContractRepository rentContractRepository, MBRentOrderService rentOrderService) {
+        this.rentContractRepository = rentContractRepository;
+        this.rentOrderService = rentOrderService;
+    }
+
+    @Override
+    public Collection<MBRentContractType> generateOrders() {
+        System.out.println(this.rentContractRepository.findMBRentContractTypesByNextOrderDateBefore(LocalDate.now()).stream().map(b -> b.getCode()).collect(Collectors.toList()));
+        return this.rentContractRepository.findMBRentContractTypesByNextOrderDateBefore(LocalDate.now()).stream()
+                .filter(ItemType::isActive)
+                .filter(contract -> contract.getRentProperty().getCurrentContract() == contract)
+                .peek(this::createOrdersAndSchedule)
+                .map(rentContractRepository::save)
+                .collect(Collectors.toList());
+    }
+
+    protected void createOrdersAndSchedule(MBRentContractType contract) {
+        while (this.canScheduleNextOrder(contract)) {
+            createOrder(contract);
+            this.scheduleNextOrderDate(contract);
+        }
+    }
+
+    protected boolean canScheduleNextOrder(final MBRentContractType contract) {
+        return contract.getRentProperty().getCurrentContract() != null
+                && contract.getNextOrderDate().isBefore(LocalDate.now());
+    }
+
+    protected void createOrder(final MBRentContractType contract) {
+        final LocalDate orderDate = contract.getNextOrderDate();
+        final ChronoUnit unit = RENT_UNIT_SCALE.getOrDefault(contract.getUnit(), DAYS);
+        final MBRentOrderType order = new MBRentOrderType();
+        order.setContract(contract);
+        order.setRentProperty(contract.getRentProperty());
+        order.setFrom(orderDate.minus(1, unit));
+        order.setTo(orderDate);
+        order.setOrderDate(LocalDate.now());
+        order.setClient(contract.getClient());
+        rentOrderService.placeOrder(order);
+    }
+
+    protected void scheduleNextOrderDate(final MBRentContractType contract) {
+        if (contract.getNextOrderDate().isBefore(contract.getTo())) {
+            setNextOrderDate(contract);
+        } else {
+            contract.getRentProperty().setCurrentContract(null);
+        }
+    }
+
+    private void setNextOrderDate(MBRentContractType contract) {
+        LocalDate nextOrderDate = contract.getNextOrderDate()
+                .plus(1, RENT_UNIT_SCALE.getOrDefault(contract.getUnit(), DAYS));
+
+        while (nextOrderDate.isAfter(contract.getTo())) {
+            nextOrderDate = nextOrderDate.minusDays(1);
+        }
+        contract.setNextOrderDate(nextOrderDate);
+    }
+}
